ddc0834b920080a50d0534ca5656b94d0f6b443a
Chokun Rojanapron <chokunplayz@ckl.moe>
2025-02-17 08:21:48 +0700

    lang: added Thai (ภาษาไทย)

---
+++ web/src/i18n/locales/th.ts
@@ -0,0 +1,262 @@
+const en = {
+    translation: {
+      head: {
+        desktop: 'Remote Desktop',
+        login: 'Login',
+        changePassword: 'Change Password',
+        terminal: 'Terminal',
+        wifi: 'Wi-Fi'
+      },
+      auth: {
+        login: 'Login',
+        placeholderUsername: 'Username',
+        placeholderPassword: 'Password',
+        placeholderPassword2: 'Please enter password again',
+        noEmptyUsername: 'Username required',
+        noEmptyPassword: 'Password required',
+        noAccount: 'Failed to get user information, please refresh web page or reset password',
+        invalidUser: 'Invalid username or password',
+        error: 'Unexpected error',
+        changePassword: 'Change Password',
+        changePasswordDesc: 'For the security of your device, please change the password!',
+        differentPassword: 'Passwords do not match',
+        illegalUsername: 'Username contains illegal characters',
+        illegalPassword: 'Password contains illegal characters',
+        forgetPassword: 'Forgot Password',
+        ok: 'Ok',
+        cancel: 'Cancel',
+        loginButtonText: 'Login',
+        tips: {
+          reset1:
+            'To reset the passwords, pressing and holding the BOOT button on the NanoKVM for 10 seconds.',
+          reset2: 'For detailed steps, please consult this document:',
+          reset3: 'Web default account:',
+          reset4: 'SSH default account:',
+          change1: 'Please note that this action will change the following passwords:',
+          change2: 'Web login password',
+          change3: 'System root password (SSH login password)',
+          change4: 'To reset the passwords, press and hold the BOOT button on the NanoKVM.'
+        }
+      },
+      wifi: {
+        title: 'Wi-Fi',
+        description: 'Configure Wi-Fi for NanoKVM',
+        success: 'Please check the network status of NanoKVM and visit the new IP address.',
+        failed: 'Operation failed, please try again.',
+        confirmBtn: 'Ok',
+        finishBtn: 'Finished'
+      },
+      screen: {
+        video: 'Video Mode',
+        resolution: 'Resolution',
+        auto: 'Automatic',
+        autoTips:
+          "Screen tearing or mouse offset may occur at specific resolutions. Consider adjusting the remote host's resolution or disable automatic mode.",
+        fps: 'FPS',
+        customizeFps: 'Customize',
+        quality: 'Quality',
+        qualityLossless: 'Lossless',
+        qualityHigh: 'High',
+        qualityMedium: 'Medium',
+        qualityLow: 'Low',
+        frameDetect: 'Frame Detect',
+        frameDetectTip:
+          "Calculate the difference between frames. Stop transmitting video stream when no changes are detected on the remote host's screen.",
+        resetHdmi: 'Reset HDMI'
+      },
+      keyboard: {
+        paste: 'Paste',
+        tips: 'Only standard keyboard letters and symbols are supported',
+        placeholder: 'Please input',
+        submit: 'Submit',
+        virtual: 'Keyboard',
+        ctrlaltdel: 'Ctrl+Alt+Del'
+      },
+      mouse: {
+        default: 'Default cursor',
+        pointer: 'Pointer cursor',
+        cell: 'Cell cursor',
+        text: 'Text cursor',
+        grab: 'Grab cursor',
+        hide: 'Hide cursor',
+        mode: 'Mouse mode',
+        absolute: 'Absolute mode',
+        relative: 'Relative mode',
+        requestPointer: 'Using relative mode. Please click desktop to get mouse pointer.',
+        resetHid: 'Reset HID'
+      },
+      image: {
+        title: 'Image',
+        loading: 'Loading...',
+        empty: 'Nothing Found',
+        mountFailed: 'Mount Failed',
+        mountDesc:
+          "In some systems, it's necessary to eject the virtual disk on the remote host before mounting the image.",
+        tips: {
+          title: 'How to upload',
+          usb1: 'Connect the NanoKVM to your computer via USB.',
+          usb2: 'Ensure that the virtual disk is mounted (Settings - Virtual Disk).',
+          usb3: 'Open the virtual disk on your computer and copy the image file to the root directory of the virtual disk.',
+          scp1: 'Make sure the NanoKVM and your computer are on the same local network.',
+          scp2: 'Open a terminal on your computer and use the SCP command to upload the image file to the /data directory on the NanoKVM.',
+          scp3: 'Example: scp your-image-path root@your-nanokvm-ip:/data',
+          tfCard: 'TF Card',
+          tf1: 'This method is supported on Linux system',
+          tf2: 'Get TF card from the NanoKVM (for the FULL version, disassemble the case first).',
+          tf3: 'Insert the TF card into a card reader and connect it to your computer.',
+          tf4: 'Copy the image file to the /data directory on the TF card.',
+          tf5: 'Insert the TF card into the NanoKVM.'
+        }
+      },
+      script: {
+        title: 'Script',
+        upload: 'Upload',
+        run: 'Run',
+        runBackground: 'Run Background',
+        runFailed: 'Run failed',
+        attention: 'Attention',
+        delDesc: 'Are you sure to delete this file?',
+        confirm: 'Yes',
+        cancel: 'No',
+        delete: 'Delete',
+        close: 'Close'
+      },
+      terminal: {
+        title: 'Terminal',
+        nanokvm: 'NanoKVM Terminal',
+        serial: 'Serial Port Terminal',
+        serialPort: 'Serial Port',
+        serialPortPlaceholder: 'Please enter the serial port',
+        baudrate: 'Baud rate',
+        confirm: 'Ok'
+      },
+      wol: {
+        title: 'Wake-on-LAN',
+        sending: 'Sending command...',
+        sent: 'Command sent',
+        input: 'Please enter the MAC',
+        ok: 'Ok'
+      },
+      download: {
+        title: 'Download Image',
+        input: 'Please enter a remote image URL',
+        ok: 'Ok',
+        disabled: '/data partition is RO, so we cannot download the image'
+      },
+      power: {
+        title: 'Power',
+        reset: 'Reset',
+        power: 'Power',
+        powerShort: 'Power (short click)',
+        powerLong: 'Power (long click)'
+      },
+      settings: {
+        title: 'Settings',
+        about: {
+          title: 'About NanoKVM',
+          information: 'Information',
+          ip: 'IP',
+          mdns: 'mDNS',
+          application: 'Application Version',
+          applicationTip: 'NanoKVM web application version',
+          image: 'Image Version',
+          imageTip: 'NanoKVM system image version',
+          deviceKey: 'Device Key',
+          community: 'Community'
+        },
+        appearance: {
+          title: 'Appearance',
+          display: 'Display',
+          language: 'Language',
+          menuBar: 'Menu Bar',
+          menuBarDesc: 'Display icons in the menu bar'
+        },
+        device: {
+          title: 'Device',
+          oled: {
+            title: 'OLED',
+            description: 'OLED screen automatically sleep',
+            0: 'Never',
+            15: '15 sec',
+            30: '30 sec',
+            60: '1 min',
+            180: '3 min',
+            300: '5 min',
+            600: '10 min',
+            1800: '30 min',
+            3600: '1 hour'
+          },
+          wifi: {
+            title: 'Wi-Fi',
+            description: 'Configure Wi-Fi',
+            setBtn: 'Config'
+          },
+          ssh: {
+            description: 'Enable SSH remote access',
+            tip: 'Set a strong password before enabling (Account - Change Password)'
+          },
+          disk: 'Virtual Disk',
+          diskDesc: 'Mount virtual U-disk on the remote host',
+          network: 'Virtual Network',
+          networkDesc: 'Mount virtual network card on the remote host'
+        },
+        tailscale: {
+          title: 'Tailscale',
+          memory: {
+            title: 'Memory optimization',
+            tip: "When memory usage exceeds the limit, garbage collection is performed more aggressively to attempt to free up memory. it's recommended to set to 50MB if using Tailscale. A Tailscale restart is required for the change to take effect.",
+            disable: 'Disable'
+          },
+          restart: 'Are you sure to restart Tailscale?',
+          stop: 'Are you sure to stop Tailscale?',
+          stopDesc: 'Log out Tailscale and disable automatic startup on boot.',
+          loading: 'Loading...',
+          notInstall: 'Tailscale not found! Please install.',
+          install: 'Install',
+          installing: 'Installing',
+          failed: 'Install failed',
+          retry: 'Please refresh and try again. Or try to install manually',
+          download: 'Download the',
+          package: 'installation package',
+          unzip: 'and unzip it',
+          upTailscale: 'Upload tailscale to NanoKVM directory /usr/bin/',
+          upTailscaled: 'Upload tailscaled to NanoKVM directory /usr/sbin/',
+          refresh: 'Refresh current page',
+          notLogin:
+            'The device has not been bound yet. Please login and bind this device to your account.',
+          urlPeriod: 'This url is valid for 10 minutes',
+          login: 'Login',
+          loginSuccess: 'Login Success',
+          enable: 'Enable Tailscale',
+          deviceName: 'Device Name',
+          deviceIP: 'Device IP',
+          account: 'Account',
+          logout: 'Logout',
+          logout2: 'Sure to logout?',
+          uninstall: 'Uninstall Tailscale',
+          okBtn: 'Yes',
+          cancelBtn: 'No'
+        },
+        update: {
+          title: 'Check for Updates',
+          queryFailed: 'Get version failed',
+          updateFailed: 'Update failed. Please retry.',
+          isLatest: 'You already have the latest version.',
+          available: 'An update is available. Are you sure to update?',
+          updating: 'Update started. Please wait...',
+          confirm: 'Confirm',
+          cancel: 'Cancel'
+        },
+        account: {
+          title: 'Account',
+          webAccount: 'Web Account Name',
+          password: 'Password',
+          updateBtn: 'Change',
+          logoutBtn: 'Logout'
+        }
+      }
+    }
+  };
+  
+  export default en;
+  
